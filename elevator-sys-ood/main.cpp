d5fcce510a43bde4d713ad5b3b0e7a12018c6f64
Alex Brochu <alexbrochu1@gmail.com>
2026-02-28 19:28:40 -0800

    some progress but got stuck working on elevator system
    Need to go back and split stops when going up vs. down
    This should let us ping-pong between traveling as far up as we need,
    then as far down as we need and loop again

--- elevator-sys-ood/main.cpp
+++ elevator-sys-ood/main.cpp
@@ -1,24 +1,43 @@
+#include <algorithm>
 #include <cstdio>
 #include <vector>
 
 enum class Direction { UP, DOWN };
-enum class State { CLOSED, MOVING, OPENED };
 
 struct Elevator {
-    State state;
-    Direction dir;
-
     int floor;
-    int target;
+    Direction direction;
+
+    std::vector<int> up_stops;
+    std::vector<int> down_stops;
+};
+
+class PickStrategy {
+public:
+    virtual bool NextAvailable(int floor, Direction dir, int &elevator_id) = 0;
+};
+
+class FirstFreeStrategy : public PickStrategy{
+public:
+    virtual bool NextAvailable(int floor, Direction dir, int &elevator_id) override {
+        return false;
+    }
 };
 
 class ElevatorSystem {
 public:
-    ElevatorSystem(int num_floors, int num_elevators): floors_count(num_floors) {
+    ElevatorSystem(int num_floors, int num_elevators, PickStrategy *strat): floors_count(num_floors), strategy(strat) {
         elevators.resize(num_elevators);
     }
 
     void RequestElevator(int floor, Direction dir) {
+        if (floor < 0 || floor > floors_count) {
+            return;
+        }
+
+        int elevator_id = 0;
+        strategy->NextAvailable(floor, dir, elevator_id);
+        Elevator &e = elevators[elevator_id];
     }
 
     void SelectFloor(int elevator_id, int floor) {
@@ -28,48 +47,30 @@ public:
         if (floor < 0 || floor > floors_count) {
             return;
         }
-
-        elevators[elevator_id].target = floor;
     }
 
     void Step() {
         //TODO: Any point in making these update in parallel? Probably not for 3 elevators
         // They do not impact eachother should not require too much synchronization
         for (Elevator &e : elevators) {
-            if (e.state == State::OPENED) {
-                e.state = (e.floor == e.target) ? State::CLOSED : State::MOVING;
-            }
-            else if (e.state == State::MOVING) {
-                if (e.floor == e.target) {
-                    e.state = State::OPENED;
-                } else {
-                    e.floor = (e.dir == Direction::UP) ? e.floor + 1 : e.floor - 1;
-                }
-            }
-            else if (e.state == State::CLOSED) {
-                if (e.floor != e.target) {
-                    e.state = State::MOVING;
-                }
-            }
         }
     }
 
     void DebugPrint() {
-        printf("[SYS] %zu Elevators covering %i floors: \n", elevators.size(), floors_count);
-        for (Elevator &e : elevators) {
-            printf(" - [%i] {%i} - floor %i; target - %i\n", e.state, e.dir, e.floor, e.target);
-        }
     }
 
 private:
     int floors_count;
     std::vector<Elevator> elevators;
+
+    PickStrategy *strategy;
 };
 
 int main() {
     printf("[MAIN] Elevator system online!\n");
 
-    ElevatorSystem sys(32, 3);
+    FirstFreeStrategy ffstrat;
+    ElevatorSystem sys(32, 3, &ffstrat);
     sys.DebugPrint();
 
     return 0;
